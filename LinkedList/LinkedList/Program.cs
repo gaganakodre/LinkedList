e59cb363465f784aaca6a6b32b7cfcfc79a926fa
shreegowri <gaganandh@gmail.com>
2022-06-18 11:23:58 +0530

    [shreegowri]created simple linkedlist of 56,30 and 70

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -5,6 +5,12 @@
        public static void Main(string[]args)
         {
             Console.WriteLine("welcome to linkedlist");
+            LinkedList linkedList=new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+            linkedList.Display();
+
         }
     }
 }
